6ce806eb7cbdd3f5320e8a29954806802b0b1259
raphael <raphael.vareilles@gmail.com>
2019-08-16 20:18:01 +0200

    cleanup 2

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
     <div className="App">
       <section className="section">
         <div className="container">
-          <h1 className="title">
+          <h1 className="">
             Hello World
           </h1>
-          <p className="subtitle">
+          <p className="">
             My first website with <strong>Bulma</strong>!
           </p>
         </div>
