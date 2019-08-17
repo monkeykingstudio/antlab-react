06b92ee4b00d6d300876b8e276e181c744cc9538
Raphael Vareilles <raphael.vareilles@gmail.com>
2019-08-18 00:32:03 +0200

    router hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import {useRoutes} from 'hookrouter';
+
 import 'bulma';
 import './scss/styles.scss'
 
 import Home from './components/Home/index';
+import QuatreCentQuatre from './components/404/index';
 
-function App() {
+const routes = {
+  // '/': () => <Navbar id="Login" />,
+  '/home': () => <Home id='Home'/>,
+};
+
+const App = () => {
+  const routeResult = useRoutes(routes);
   return (
-    <div className="App">
-      <Home id='Home'/>
-    </div>
+   routeResult || <QuatreCentQuatre />
   );
 }
 
---
+++ src/components/404/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+
+const QuatreCentQuatre = (props) => {
+
+    return (
+        <div id=''>
+            <span>Page not found!</span>
+        </div>
+    );
+};
+
+export default QuatreCentQuatre;
