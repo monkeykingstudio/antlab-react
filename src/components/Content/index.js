2f0ba133cab0b6a8251b25975e1a18ed6bdaf7b4
raphael <raphael.vareilles@gmail.com>
2019-08-20 17:55:37 +0200

    update layout feeder

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -7,7 +7,7 @@ const Content = (props) => {
     return (
         <div id={props.id}>
             <div className="columns">
-                <div className="column is-one-quarter">
+                <div className="column is-half-tablet is-one-third-desktop ">
                     <Feeder title='feed ants' id='Feeder'/>
                 </div>
             </div> 
