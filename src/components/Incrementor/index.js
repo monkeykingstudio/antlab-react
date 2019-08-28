e97be6d1b847f9ca9df31850dd50defe31e26311
raphael <raphael.vareilles@gmail.com>
2019-08-28 17:49:16 +0200

    almost working population

--- src/components/Incrementor/index.js
+++ src/components/Incrementor/index.js
@@ -1,11 +1,54 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './index.scss';
 import Output from './output';
 
 const Incrementor = (props) => {
 
-    const [births, setBirths] = useState(0)
-    const [death, setDeath] = useState(0)
+    let bindValue;
+    switch (props.id) {
+        case 0:
+            bindValue = props.major;
+            break;
+        case 1:
+            bindValue = props.media;
+            break;
+        case 2:
+            bindValue = props.minor;
+            break;
+        case 3:
+            bindValue = props.breed;
+            break;                                       
+        default:
+            break;
+    }
+
+    const [births, setBirths] = useState(
+        0
+    )
+    const [death, setDeath] = useState(
+        bindValue.death
+    )
+
+    useEffect(() => {
+    //    setBirths(props.major.births)
+       switch (props.id) {
+        case 0:
+            setBirths(props.major.births);
+            break;
+        case 1:
+            setBirths(props.media.births);
+            break;
+        case 2:
+            bindValue = props.minor;
+            break;
+        case 3:
+            bindValue = props.breed;
+            break;                                       
+        default:
+            break;
+    }
+      });
+
 
     const handleDeaths = () => {
         setDeath(death + 1)
@@ -15,28 +58,33 @@ const Incrementor = (props) => {
         setBirths(births + 1)        
     }
 
-    const setPopulationDeaths = (e) => {
-        console.log(props.id, 'e: ',e)
+    const setPopulationDeath = (e, f) => {
+        console.log('DEATH ID INC', f, ' e: ',e)
+        props.setPopulationDeath(e, f)
     }
 
-    const setPopulationBirths = (e) => {
-        console.log(props.id, 'e: ',e)
+    const setPopulationBirths = (e, f) => {
+        console.log('BIRTH ID INC', f, ' e: ',e)
+        props.setPopulationBirths(e, f)
     }
 
+    
+    console.log('bind value: ', bindValue)
+    console.log(births)
     return (     
         <div className='Incrementor'>
             <div className='inputs'>
                 <p className="title">{props.datas[props.id]}</p>
                 <div className="grouped-button">
                     <button 
-                        onClick={() => {
-                        setPopulationDeaths(death);
+                        onClick={(e, f) => {
+                            setPopulationDeath(death, props.id);
                         }} 
                         onMouseUp={handleDeaths} type="button">-</button>
                         
                     <button 
-                        onClick={() => {
-                        setPopulationBirths(births);
+                        onClick={(e, f) => {
+                            setPopulationBirths(births, props.id);
                         }}  
                         onMouseUp={handleBirths} type="button">+</button>
                 </div>
--- src/components/Population/index.js
+++ src/components/Population/index.js
@@ -6,26 +6,6 @@ import axios from 'axios'
 
 const Population = (props) => {
 
-    const [major, setMajor] = useState({
-        "death": 0,
-        "births": 0
-    })
-
-    const [media, setMedia] = useState({
-        "death": 0,
-        "births": 0
-    })
-
-    const [minor, setMinor] = useState({
-        "death": 0,
-        "births": 0
-    })
-
-    const [breed, setBreed] = useState({
-        "death": 0,
-        "births": 0
-    })
-
     const [data, setData] = useState({})
 
     useEffect(() => {
@@ -34,35 +14,100 @@ const Population = (props) => {
                 'http://localhost:4000/population'
             );  
             setData(result.data);
-            setMajor(data.major);
-            setMedia(data.media);
-            setMinor(data.minor);
-            setBreed(data.breed);
+
+            setMajor(result.data[0]);
+            
+            setMedia(result.data[1]);
+            setMinor(result.data[2]);
+            setBreed(result.data[3]);
         }; 
         fetchData();
     },[]);
+
+    const [major, setMajor] = useState({
+        id: 0,
+        death: 0,
+        births: 0
+    })
+
+    const [media, setMedia] = useState({
+        death: 0,
+        births: 0
+    })
+
+    const [minor, setMinor] = useState({
+        death: 0,
+        births: 0
+    })
+
+    const [breed, setBreed] = useState({
+        death: 0,
+        births: 0
+    })
+    console.log('data: ', data)
+    console.log('major: ', major)
 
     return (     
         <div id={props.id}>
            <span className="title">population</span>
            <span>248 individus</span>
            <Counter 
-        //    setPopulationDeath={
-        //     (e) => {
-        //             setEntry(
-        //                 prevState => {
-        //                     return { ...prevState, food: e }
-        //                 }
-        //             );
-        //         }
-        //    }
-        //    setPopulationBirth={}
+            setPopulationDeath={
+             (e, f) => {
+                     switch (f) {
+                         case 0:
+                            
+                            break;
+                        case 1:
+                             
+                            break;
+                        case 2:
+                             
+                            break;
+                        case 3:
+                             
+                            break;
+                         default:
+                             break;
+                     }
+                }
+           }
+            setPopulationBirths={(e, f) => {
+                switch (f) {
+                case 0:
+                    setMajor(
+                        prevState => {
+                            console.log(major)
+                            return { ...prevState, "births": e}
+                        }
+                    );
+                    break;
+                case 1:
+                        
+                    break;
+                case 2:
+                        
+                    break;
+                case 3:
+                        
+                    break;
+
+                default:
+                    break;
+                }
+            }
+           }
            entries={[
                'major',
                'media',
                'minor',
                'breed'
-           ]}/> 
+           ]}
+           major={major}
+           media={media}
+           minor={minor}
+           breed={breed}
+           /> 
         </div>
     );
 };
