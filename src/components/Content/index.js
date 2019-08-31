ff06e62989d41173e6c2bf4d3452d88e966c85be
raphael <raphael.vareilles@gmail.com>
2019-08-31 12:00:51 +0200

    minicount

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './index.scss';
-import Feeder from '../Feeder/index';
-import Population from '../Population';
+// import Feeder from '../Feeder/index';
+// import Population from '../Population';
+import MiniCount from '../MiniCount';
 
 const Content = (props) => {
 
     return (
         <div id={props.id}>
             <div className="columns">
-                <div className="column is-half-tablet is-one-third-desktop ">
+                {/* <div className="column is-half-tablet is-one-third-desktop ">
                     <Feeder title='feed ants' id='Feeder'/>
                 </div>
 
                 <div className="column is-half-tablet is-one-third-desktop ">
                     <Population title='Manage population' id='Population'/>
+                </div> */}
+
+                <div className="column is-half-tablet is-one-third-desktop ">
+                    <MiniCount title='Mini count' id='MiniCount'/>
                 </div>
 
             </div> 
--- src/components/Incrementor/index.js
+++ src/components/Incrementor/index.js
@@ -39,6 +39,8 @@ const Incrementor = (props) => {
         default:
             break;
         }
+
+        // return(console.log('bwaaaah'))
     },
     [
     props.id, 
